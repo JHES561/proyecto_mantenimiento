190f9f27d7225893dff0f42ca1808d92773a14ae
JHES561 <thejhes2006@gmail.com>
2025-11-01 10:00:51 -0500

    actualizacion

--- Formulario_dinámico/script.js
+++ Formulario_dinámico/script.js
@@ -1,3 +1,18 @@
-document.addEventListener("DOMContentLoaded", function() {
-    alert("Welcome to My First Website!");
+document.getElementById("choice").addEventListener("change", function() {
+    const value = this.value;
+    const additionalFields = document.getElementById("additionalFields");
+
+    additionalFields.innerHTML = "";
+
+    if (value === "books") {
+        additionalFields.innerHTML = `
+            <label for="author">Autor:</label>
+            <input type="text" id="author" name="author" placeholder="Nombre del autor">
+        `;
+    } else if (value === "movies") {
+        additionalFields.innerHTML = `
+            <label for="director">Director:</label>
+            <input type="text" id="director" name="director" placeholder="Nombre del director">
+        `;
+    }
 });
